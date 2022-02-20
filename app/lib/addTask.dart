8941c8bc2167fd15f5dbe841bc6271394fb54a8b
Shanemel Asuncion <shanemelasuncion@gmail.com>
2022-02-19 20:34:18 -0700

    draft2

--- app/lib/addTask.dart
+++ app/lib/addTask.dart
@@ -1,54 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class addTask extends StatelessWidget {
+  final _key = GlobalKey<FormBuilderState>();
   TextEditingController task_i = new TextEditingController();
   TextEditingController course_i = new TextEditingController();
 
-  createAlertDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text("Add Task"),
-            content: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextFormField(
-                  controller: task_i,
-                  decoration: InputDecoration(hintText: 'Name of Task'),
-                ),
-                TextFormField(
-                  controller: course_i,
-                  decoration: InputDecoration(hintText: 'Course Name'),
-                ),
-                Text("\nSubTasks:"),
-              ],
-            ),
-            actions: <Widget>[
-              MaterialButton(child: Text("Submit"), onPressed: () {})
-            ],
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      body: Builder(builder: (context) {
-        return Container(
-            child: Center(
-                child: FlatButton(
-          padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          color: Colors.blueGrey,
-          onPressed: () {
-            createAlertDialog(context);
-          },
-          child: Text("Alert"),
-        )));
-      }),
-    ));
+    return MaterialApp(
+      title: 'Student Course Manager',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Add New Task"),
+        ),
+        body: FormBuilder(
+          key: _key,
+          child: Column(children: [
+            TextFormField(
+              controller: task_i,
+              decoration: InputDecoration(hintText: 'Name of Task'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: course_i,
+              decoration: InputDecoration(hintText: 'Course Name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            FormBuilderDateRangePicker(
+              name: 'date_range',
+              firstDate: DateTime(1970),
+              lastDate: DateTime(2030),
+              decoration: InputDecoration(
+                labelText: 'Start Date-Due Date',
+              ),
+            ),
+            FormBuilderDateTimePicker(
+              name: 'date',
+              inputType: InputType.time,
+              decoration: InputDecoration(
+                labelText: 'Deadline Time',
+              ),
+              initialTime: TimeOfDay(hour: 8, minute: 0),
+            ),
+            Text("\nSubTasks:\n"),
+            ElevatedButton(
+                onPressed: () {
+                  //Send values
+                },
+                child: const Text('Submit'))
+          ]),
+        ),
+      ),
+    );
   }
 }
